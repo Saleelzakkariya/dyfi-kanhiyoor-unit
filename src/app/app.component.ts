50f22f3b27121719e3cd463ab5ef7654b2c3a900
Mohammed saleel <saleelzakkariya@gmail.com>
2020-12-01 08:39:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pdf-view';
+  src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+  ngOnInit() {
+    console.log('pdf');
+  }
 }
